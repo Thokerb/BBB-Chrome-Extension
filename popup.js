9446292f021b3d2e8b1ad8f97f98652c7f81c062
Thomas Anton Kerber <Thomas.Kerber@student.uibk.ac.at>
2021-04-15 12:02:53 +0200

    init commit

---
+++ popup.js
@@ -0,0 +1,28 @@
+// Initialize butotn with users's prefered color
+let changeColor = document.getElementById("audioRange");
+
+changeColor.addEventListener("change", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  value = document.getElementById("audioRange").value;
+  chrome.storage.sync.set({audioValue: value });
+
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setAudio,
+  });
+});
+
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function setAudio() {
+  chrome.storage.sync.get("audioValue", ({ audioValue }) => {
+    var audioTag = document.getElementById("remote-media");
+
+    if(audioTag !== null){
+      audioTag.volume = audioValue;
+    }
+  
+  });
+}
